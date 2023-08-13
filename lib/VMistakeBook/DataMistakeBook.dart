791e86457e18de6ad0a6f6b58c4422adffdfc961
InternationaleDefy <DTEye1533014901@126.com>
2023-08-13 20:04:39 +0800

    Code view, New tasks

--- lib/VMistakeBook/DataMistakeBook.dart
+++ lib/VMistakeBook/DataMistakeBook.dart
@@ -40,7 +40,8 @@ class DataMistakeBookListItem {
 Future<DataMistakeBook> getGetDataMistakeBook() async {
   final token = AuthritionState.instance.getToken();
   // final token = 'soe-token-eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzbWFydC1vcmFsLWV2YWx1YXRpb24iLCJsb2dpblVzZXIiOnsiYWNjb3VudE5vIjoidXNlcl8xNjAxODQ0ODU0Nzk3NzY2NjU2IiwiaWRlbnRpZnlJZCI6bnVsbCwicm9sZUlkIjpudWxsLCJuaWNrTmFtZSI6Ikpvc2h1YSIsInJlYWxOYW1lIjoi5p2D6ZmQ6K6k6K-B5rWL6K-V6LSm5Y-3IiwiZmlyc3RMYW5ndWFnZSI6bnVsbCwicGhvbmUiOiIxMzU3Njg0NTM1NCIsIm1haWwiOiIxNzY1OTQ3NDI0QHFxLmNvbSJ9LCJpYXQiOjE2OTA5ODUwNjcsImV4cCI6MTY5MTU4OTg2N30.GH1K5fSRB8237XzQoNJiXgqtCUZrjB4fddpxHOAn7DE';
-  final uri = Uri.parse("http://47.101.58.72:8888/corpus-server/api/mistake/v1/getDetail?oneWeekKey=0");
+  final uri = Uri.parse(
+      "http://47.101.58.72:8888/corpus-server/api/mistake/v1/getDetail?oneWeekKey=0");
   final response = await http.Client().get(
     uri,
     headers: {
@@ -48,7 +49,9 @@ Future<DataMistakeBook> getGetDataMistakeBook() async {
     },
   );
   final u8decoded = utf8.decode(response.bodyBytes);
-  final decoded = jsonDecode(u8decoded);
+  // COMMENT-8-12 服务器上目前没有我们测试账号的错题数据, 这是用来测试的临时数据, 不用注释掉即可. 不必删除
+  final decoded = jsonDecode(
+      '{    "code": 0,    "data": {        "eachMistakeTypeNumber": [             {                "mistakeNum": 3,                "mistakeTypeName": "语音评测",                "mistakeTypeCode": 0             }        ],        "mistakeTotalNumber": 3,        "stubbornMistakeNumber": 1     },    "msg": null}');
   final code = decoded['code'];
   if (code != 0) throw ("wrong return code");
   final mistakeBookListItem = decoded['data']['eachMistakeTypeNumber'];
@@ -67,13 +70,13 @@ class DataMistakeDetail {
 }
 
 class DataMistakeDetailListItem {
-  String? cpsrcdId; //题目的快照id，为了识别用户答的是哪道题，答题时给后端传答题结果时需要带上它
-  String? corpusId;
-  String? cpsgrpId;
-  String? topicId;
-  int? evalMode;
-  int? difficulty;
-  int? wordWeight;
+  String cpsrcdId; //题目的快照id，为了识别用户答的是哪道题，答题时给后端传答题结果时需要带上它
+  String corpusId;
+  String cpsgrpId;
+  String topicId;
+  int evalMode;
+  int difficulty;
+  int wordWeight;
   String pinyin;
   String refText;
   String audioUrl;
@@ -91,24 +94,25 @@ class DataMistakeDetailListItem {
   });
   factory DataMistakeDetailListItem.fromJson(Map<String, dynamic> json) {
     return DataMistakeDetailListItem(
-      cpsrcdId: json['cpsrcdId'],
-      corpusId: json['corpusId'],
-      cpsgrpId: json['cpsgrpId'],
-      topicId: json['topicId'],
-      evalMode: json['evalMode'],
-      difficulty: json['difficulty'],
-      wordWeight: json['wordWeight'],
-      pinyin: json['pinyin'],
-      refText: json['refText'],
-      audioUrl: json['audioUrl'],
+      cpsrcdId: json['cpsrcdId'] ?? "",
+      corpusId: json['corpusId'] ?? "",
+      cpsgrpId: json['cpsgrpId'] ?? "",
+      topicId: json['topicId'] ?? "",
+      evalMode: json['evalMode'] ?? 0,
+      difficulty: json['difficulty'] ?? 0,
+      wordWeight: json['wordWeight'] ?? 0,
+      pinyin: json['pinyin'] ?? "",
+      refText: json['refText'] ?? "",
+      audioUrl: json['audioUrl'] ?? "",
     );
   }
 }
 
-Future<DataMistakeDetail> postGetDataMistakeDetail(int mistakeTypeCode, int oneWeekKey) async {
+Future<DataMistakeDetail> postGetDataMistakeDetail(
+    int mistakeTypeCode, int oneWeekKey) async {
   final token = AuthritionState.instance.getToken();
-  // final token = 'soe-token-eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzbWFydC1vcmFsLWV2YWx1YXRpb24iLCJsb2dpblVzZXIiOnsiYWNjb3VudE5vIjoidXNlcl8xNjAxODQ0ODU0Nzk3NzY2NjU2IiwiaWRlbnRpZnlJZCI6bnVsbCwicm9sZUlkIjpudWxsLCJuaWNrTmFtZSI6Ikpvc2h1YSIsInJlYWxOYW1lIjoi5p2D6ZmQ6K6k6K-B5rWL6K-V6LSm5Y-3IiwiZmlyc3RMYW5ndWFnZSI6bnVsbCwicGhvbmUiOiIxMzU3Njg0NTM1NCIsIm1haWwiOiIxNzY1OTQ3NDI0QHFxLmNvbSJ9LCJpYXQiOjE2OTA5ODUwNjcsImV4cCI6MTY5MTU4OTg2N30.GH1K5fSRB8237XzQoNJiXgqtCUZrjB4fddpxHOAn7DE';
-  final uri = Uri.parse('http://47.101.58.72:8888/corpus-server/api/mistake/v1/getMistakes');
+  final uri = Uri.parse(
+      'http://47.101.58.72:8888/corpus-server/api/mistake/v1/getMistakes');
   final response = await http.Client().post(
     uri,
     body: jsonEncode({
@@ -123,19 +127,14 @@ Future<DataMistakeDetail> postGetDataMistakeDetail(int mistakeTypeCode, int oneW
       'utf-8',
     ),
   );
-  final u8decoded = utf8.decode(response.bodyBytes);
-  final decoded = jsonDecode(u8decoded);
-  final code = decoded['code'];
+  final parsedJson = jsonDecode(
+      '{    "code": 0,    "data": [        {            "cpsrcdId": "cpsrcd_1667734326118322176",            "corpusId": null,            "cpsgrpId": "cpsgrp_1666820615325224960",            "topicId": "topic_1667733584137555968",            "evalMode": null,            "difficulty": -1,            "wordWeight": 0,            "pinyin": "di4 san1 sheng1    di4 er4 sheng1  ",            "refText": "第三声 第二声",            "audioUrl": "https://soe-oss.oss-cn-shanghai.aliyuncs.com/corpus/2023/06/14/5c1a801c02c740718616df3fa28918ab.MP3",            "tags": null,            "cNum": 8         },        {            "cpsrcdId": "cpsrcd_1667734692889235456",            "corpusId": null,            "cpsgrpId": "cpsgrp_1666820615325224960",            "topicId": "topic_1667734588706918400",            "evalMode": null,            "difficulty": -1,            "wordWeight": 0,            "pinyin": "di4 yi1 sheng1 + di4 yi1 sheng1     di4 yi1 sheng1 + di4 si4 sheng1   ",            "refText": "第一声+第一声  第一声+第四声 ",            "audioUrl": "https://soe-oss.oss-cn-shanghai.aliyuncs.com/corpus/2023/06/14/905990689126453a93bde760bdd4506a.MP3",            "tags": null,            "cNum": 1        },        {            "cpsrcdId": "cpsrcd_1667734773675724800",            "corpusId": null,            "cpsgrpId": "cpsgrp_1666820615325224960",            "topicId": "topic_1667734588706918400",            "evalMode": null,            "difficulty": -1,            "wordWeight": 0,            "pinyin": "di4 yi1 sheng1 + di4 er4 sheng1  di4 er4 sheng1 + di4 er4 sheng1 ",            "refText": "第一声+第二声第二声+第二声",            "audioUrl": "https://soe-oss.oss-cn-shanghai.aliyuncs.com/corpus/2023/06/14/aa8b1f982c234fd1b8fc634f23b8368e.MP3",            "tags": null,            "cNum": 2        }    ],    "msg": null}        ');
+  final code = parsedJson['code'];
   if (code != 0) throw ('wrong return code');
-
-  // final parsedJson = jsonDecode(
-  //     '{    "code": 0,    "data": [        {            "cpsrcdId": "cpsrcd_1667734326118322176",            "corpusId": null,            "cpsgrpId": "cpsgrp_1666820615325224960",            "topicId": "topic_1667733584137555968",            "evalMode": null,            "difficulty": -1,            "wordWeight": 0,            "pinyin": "di4 san1 sheng1    di4 er4 sheng1  ",            "refText": "第三声 第二声",            "audioUrl": "https://soe-oss.oss-cn-shanghai.aliyuncs.com/corpus/2023/06/14/5c1a801c02c740718616df3fa28918ab.MP3",            "tags": null,            "cNum": 8         },        {            "cpsrcdId": "cpsrcd_1667734692889235456",            "corpusId": null,            "cpsgrpId": "cpsgrp_1666820615325224960",            "topicId": "topic_1667734588706918400",            "evalMode": null,            "difficulty": -1,            "wordWeight": 0,            "pinyin": "di4 yi1 sheng1 + di4 yi1 sheng1     di4 yi1 sheng1 + di4 si4 sheng1   ",            "refText": "第一声+第一声  第一声+第四声 ",            "audioUrl": "https://soe-oss.oss-cn-shanghai.aliyuncs.com/corpus/2023/06/14/905990689126453a93bde760bdd4506a.MP3",            "tags": null,            "cNum": 1        },        {            "cpsrcdId": "cpsrcd_1667734773675724800",            "corpusId": null,            "cpsgrpId": "cpsgrp_1666820615325224960",            "topicId": "topic_1667734588706918400",            "evalMode": null,            "difficulty": -1,            "wordWeight": 0,            "pinyin": "di4 yi1 sheng1 + di4 er4 sheng1  di4 er4 sheng1 + di4 er4 sheng1 ",            "refText": "第一声+第二声第二声+第二声",            "audioUrl": "https://soe-oss.oss-cn-shanghai.aliyuncs.com/corpus/2023/06/14/aa8b1f982c234fd1b8fc634f23b8368e.MP3",            "tags": null,            "cNum": 2        }    ],    "msg": null}        ');
-  // final code = parsedJson['code'];
-  // final data = parsedJson['data'];
-  final data = decoded['data'];
-  List<DataMistakeDetailListItem> listMistakeDetail = List.empty(growable: true);
-  for (final item in data) 
+  final data = parsedJson['data'];
+  List<DataMistakeDetailListItem> listMistakeDetail =
+      List.empty(growable: true);
+  for (final item in data)
     listMistakeDetail.add(DataMistakeDetailListItem.fromJson(item));
-  print(listMistakeDetail);
   return DataMistakeDetail(listMistakeDetail: listMistakeDetail);
 }
--- lib/VMistakeBook/ViewMistakeDetail.dart
+++ lib/VMistakeBook/ViewMistakeDetail.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:project_soe/CComponents/ComponentAppBar.dart';
 import 'package:project_soe/CComponents/ComponentBottomNavigation.dart';
+import 'package:project_soe/CComponents/ComponentRoundButton.dart';
 import 'package:project_soe/CComponents/ComponentTitle.dart';
 import 'package:project_soe/GGlobalParams/Styles.dart';
 import 'package:project_soe/VMistakeBook/DataMistakeBook.dart';
@@ -19,9 +22,51 @@ class ViewMistakeDetail extends StatelessWidget {
   }
 
   Widget _buildItem(DataMistakeDetailListItem listItem) {
-    return ComponentTitle(
-      label: 'cpsrcdId${listItem.cpsrcdId!}',
-      style: gTitleStyle,
+    return Container(
+      height: 240,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ComponentTitle(
+              label: '题目ID:${listItem.cpsrcdId}', style: gInfoTextStyle0),
+          ComponentTitle(
+              label: '难度:${listItem.difficulty} ', style: gInfoTextStyle0),
+          ComponentTitle(
+              label: '分数:${listItem.wordWeight} ', style: gInfoTextStyle0),
+          ComponentTitle(
+              label: '拼音:${listItem.pinyin} ', style: gInfoTextStyle0),
+          ComponentTitle(
+              label: '原文:${listItem.refText} ', style: gInfoTextStyle0),
+          ComponentTitle(
+              label: '音频:${listItem.audioUrl}', style: gInfoTextStyle0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ComponentRoundButton(
+                func: () {
+                  // TODO-23-8-13 进入答题界面
+                  // 语音输入答题参考 ComponentVoiceInput
+                  // ViewExam, ViewPractice
+                  // 1. 复用ViewExam如何ComponentVoiceInput, Data(Msg)Question
+                  // 2. 参考ViewPractice如何开启答题界面, 如何和返回参数
+                },
+                color: gColorE3EDF7RGBA,
+                child: ComponentTitle(
+                  label: '点击答题',
+                  style: gInfoTextStyle0,
+                ),
+                height: 25,
+                width: 25,
+                radius: 3,
+              ),
+            ],
+          ),
+          Divider(
+            thickness: 2.0,
+          ),
+        ],
+      ),
     );
   }
 
